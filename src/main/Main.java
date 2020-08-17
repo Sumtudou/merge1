4c6eeeb2aa7b095007913878b233e13db54258d8
Sumtudou <1163015195@qq.com>
2020-08-17 23:05:13 +0800

    [merge][Jay] feature:modify main and test1

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("I am Jay ,I add main.java");
         System.out.println("I am Jay ,I add main.java");
-        System.out.println("I am Jay ,I add main.java");
+
+        System.out.println("I am Jay ,I modify main");
+        System.out.println("I am Jay ,I modify main");
+        System.out.println("I am Jay ,I modify main");
     }
 }
--- src/main/Test1.java
+++ src/main/Test1.java
@@ -4,6 +4,9 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("I am Jay ,I add Test1.java");
         System.out.println("I am Jay ,I add Test1.java");
-        System.out.println("I am Jay ,I add Test1.java");
+
+        System.out.println("I am Jay ,I modify Test1");
+        System.out.println("I am Jay ,I modify Test1");
+        System.out.println("I am Jay ,I modify Test1");
     }
 }
