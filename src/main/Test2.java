eb37ec44288f120c189efbe21a423df16f9ecf8e
Sumtudou <1163015195@qq.com>
2020-08-18 01:21:09 +0800

    [merge][Jay] feature:modify test2 in test branch again

--- src/main/Test2.java
+++ src/main/Test2.java
@@ -4,14 +4,8 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("I am Jay ,I add test2");
         System.out.println("I am Jay ,I add test2");
-        System.out.println("I am Jay ,I add test2");
+        System.out.println("I am Jay ,I add test2,and modify here");
 
-        System.out.println("I am in test branch");
-        System.out.println("I am in test branch");
-        System.out.println("I am in test branch");
-
-        System.out.println("I modify test branch again");
-        System.out.println("I modify test branch again");
         System.out.println("I modify test branch again");
     }
 }
