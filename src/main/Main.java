6fc8dd6316d75e3b5541348327ce9ffb9e274a10
Sumtudou <1163015195@qq.com>
2020-08-17 22:55:17 +0800

    [merge][Jay] feature: add main test1 test2.java

---
+++ src/main/Main.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I am Jay ,I add main.java");
+        System.out.println("I am Jay ,I add main.java");
+        System.out.println("I am Jay ,I add main.java");
+    }
+}
---
+++ src/main/Test1.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("I am Jay ,I add Test1.java");
+        System.out.println("I am Jay ,I add Test1.java");
+        System.out.println("I am Jay ,I add Test1.java");
+    }
+}
