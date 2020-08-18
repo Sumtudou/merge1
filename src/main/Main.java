c5b83395b21b0d8ff7d200d72a17abc5e4706b6e
geticsen <2831367585@qq.com>
2020-08-18 17:43:35 +0800

    [merge][Jay] refactor:Jay modify main ,test1

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,8 +4,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("I am Jay ,I add main.java");
         System.out.println("I am Jay ,I add main.java");
-        System.out.println("I am Jay ,I add main.java");
 
-        System.out.println("I am Caps, I delete some line,and add this modify Main");
+        System.out.println("I am Jay, I modify Main ");
+        System.out.println("I am Jay, I modify Main ");
+        System.out.println("I am Jay, I modify Main ");
+        System.out.println("I am Jay, I modify Main ");
+        System.out.println("I am Jay, I modify Main ");
     }
 }
--- src/main/Test1.java
+++ src/main/Test1.java
@@ -3,21 +3,13 @@ package main;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("I am Jay ,I add Test1.java");
-        System.out.println("I am Jay ,I add Test1.java");
-        System.out.println("I am Jay ,I add Test1.java");
 
-        System.out.println("I am green ,I modify Test1");
+        System.out.println("I am Jay, I modify Test1 ");
+        System.out.println("I am Jay, I modify Test1 ");
+        System.out.println("I am Jay, I modify Test1 ");
+        System.out.println("I am Jay, I modify Test1 ");
+        System.out.println("I am Jay, I modify Test1 ");
+        System.out.println("I am Jay, I modify Test1 ");
 
-        System.out.println("I am caps, I change Test1");
-        System.out.println("I am caps, I change Test1");
-        System.out.println("I am caps, I change Test1");
-
-        System.out.println("I am green, I modify test1 for rebase");
-        System.out.println("I am green, I modify test1 for rebase");
-        System.out.println("I am green, I modify test1 for rebase");
-
-        System.out.println("I am green, I modify test1 for rebase second");
-        System.out.println("I am green, I modify test1 for rebase second");
-        System.out.println("I am green, I modify test1 for rebase second");
     }
 }
