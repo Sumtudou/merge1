05ac22195979c207ac17769e234e478f3686673d
geticsen <2831367585@qq.com>
2020-08-18 17:55:08 +0800

    [merge][Jay] refactor:see the conflict

--- src/main/Test1.java
+++ src/main/Test1.java
@@ -5,7 +5,7 @@ public class Test1 {
         System.out.println("I am Jay ,I add Test1.java");
         System.out.println("I am ffff ,I add Test1.java");
 
-        System.out.println("I am eeee ,I add Test1.java");
-
+        System.out.println("I am jayjaay, I modify test1 for rebase second");
+        System.out.println("I am green, I modify test1 for rebase second");
     }
 }
