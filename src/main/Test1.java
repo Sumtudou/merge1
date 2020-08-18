2e83d54e7e4bf94e9b00013f133a21365eef731e
geticsen <2831367585@qq.com>
2020-08-18 12:52:19 +0800

    [merge][green] refactor: green change Test1 first

--- src/main/Test1.java
+++ src/main/Test1.java
@@ -6,23 +6,8 @@ public class Test1 {
         System.out.println("I am Jay ,I add Test1.java");
         System.out.println("I am Jay ,I add Test1.java");
 
-        System.out.println("I am green ,I modify Test1");
-        System.out.println("I am green ,I modify Test1");
-        System.out.println("I am green ,I modify Test1");
-
-
-        System.out.println("I am Jay ,I modify Test1");
-        System.out.println("I am Jay ,I modify Test1");
-        System.out.println("I am Jay ,I modify Test1");
-
-        System.out.println("I am Jay ,I modify Test1 again");
-        System.out.println("I am Jay ,I modify Test1 again");
-        System.out.println("I am Jay ,I modify Test1 again");
-
-        System.out.println("I am Green ,I modify Test111"); // green modify this
-
-        System.out.println("I am green, I am so angry");
-        System.out.println("I am green, I am so angry");
-        System.out.println("I am green, I am so angry");
+        System.out.println("I am green, I modify test1 for rebase");
+        System.out.println("I am green, I modify test1 for rebase");
+        System.out.println("I am green, I modify test1 for rebase");
     }
 }
