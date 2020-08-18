5dce7265b2e0164714c69dcda7228519ce6ada07
caps <yzsylinder@163.com>
2020-08-18 17:40:39 +0800

    [merge][Green] refactor:change Main ,Test1

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,9 +3,9 @@ package main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("I am Jay ,I add main.java");
-        System.out.println("I am Jay ,I add main.java");
-        System.out.println("I am Jay ,I add main.java");
 
-        System.out.println("I am Caps, I delete some line,and add this modify Main");
+        System.out.println("I am Green , I modify Main");
+        System.out.println("I am Green , I modify Main");
+        System.out.println("I am Green , I modify Main");
     }
 }
