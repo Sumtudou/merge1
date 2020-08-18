b1e68d91069625b3df6764081e21eb9060824a15
geticsen <2831367585@qq.com>
2020-08-18 18:02:41 +0800

    [merge][Jay] refactor:in master branch change test2

--- src/main/Test2.java
+++ src/main/Test2.java
@@ -3,14 +3,10 @@ package main;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("I am Jay ,I add test2");
-        System.out.println("I am Jay ,I add test2");
-        System.out.println("I am Jay ,I add test2,here");
 
-        System.out.println("I am in master branch, I change test2");
-        System.out.println("I am in master branch, I change test2");
-        System.out.println("I am in master branch, I change test2");
-        System.out.println("I am in test Branch, and change Test2");
-        System.out.println("I am in test Branch, and change Test2");
-        System.out.println("I am in test Branch, and change Test2");
+        System.out.println("I am Jay ,master change test2");
+        System.out.println("I am Jay ,master change test2");
+        System.out.println("I am Jay ,master change test2");
+
     }
 }
